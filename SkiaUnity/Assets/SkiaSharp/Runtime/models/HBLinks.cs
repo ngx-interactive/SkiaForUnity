d2fd0d9bad32de6b93cf99bcbfd3cb0e09993c96
pleiteNgx <126503793+pleiteNgx@users.noreply.github.com>
2026-04-23 11:52:50 -0700

    Removing duplicated files

--- SkiaUnity/Assets/SkiaSharp/Runtime/models/HBLinks.cs
+++
@@ -1,8 +0,0 @@
-namespace SkiaSharp.Unity {
-    public class HBLinks {
-        public int IndexStart;
-        public int IndexEnd;
-        public int Length;
-        public string link;
-    }
-}
